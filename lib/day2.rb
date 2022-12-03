088755c8c7e40b4bc0b8ff0814192450ca44f185
Tim Peters <tim@catminion.net>
2022-12-03 14:32:44 +1030

    Add day 2 part 2

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,8 +1,13 @@
 class Day2 < Base
   class Round
-    def initialize(moves)
+    def initialize(moves, part)
       @opp_move = map_move(moves[0])
-      @my_move = map_move(moves[1])
+      @my_move =
+        if part == 1
+          map_move(moves[1])
+        else
+          choose_move(moves[1])
+        end
     end
 
     BEATS = {
@@ -29,6 +34,14 @@ class Day2 < Base
       shape_score + outcome_score
     end
 
+    def choose_move(instruction)
+      case instruction
+      when "X" then BEATS[@opp_move]        # need to lose
+      when "Y" then @opp_move               # need to draw
+      when "Z" then BEATS.invert[@opp_move] # need to win
+      end
+    end
+
     def map_move(move)
       case move
       when "A", "X" then :rock
@@ -41,10 +54,14 @@ class Day2 < Base
   end
 
   def part1
-    parse_input.map(&:score).sum
+    parse_input.map { |moves| Round.new(moves, 1) }.map(&:score).sum
+  end
+
+  def part2
+    parse_input.map { |moves| Round.new(moves, 2) }.map(&:score).sum
   end
 
   def parse_input
-    @parse_input ||= raw_input.each_line.map(&:split).map { |moves| Round.new(moves) }
+    @parse_input ||= raw_input.each_line.map(&:split)
   end
 end
--- test/day2_test.rb
+++ test/day2_test.rb
@@ -13,4 +13,8 @@ describe Day2 do
   def test_part1
     assert_equal 15, @d.part1
   end
+
+  def test_part2
+    assert_equal 12, @d.part2
+  end
 end
