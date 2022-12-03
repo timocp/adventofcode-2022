e015d496f80052f84578a1da53018c26070a410d
Tim Peters <tim@catminion.net>
2022-12-03 14:02:59 +1030

    Add day 1 part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -3,6 +3,10 @@ class Day1 < Base
     parse_input.map(&:sum).max
   end
 
+  def part2
+    parse_input.map(&:sum).sort.last(3).sum
+  end
+
   def parse_input
     @parse_input ||= raw_input.split(/\n\n/).map { |group| group.split.map(&:to_i) }
   end
--- test/day1_test.rb
+++ test/day1_test.rb
@@ -24,4 +24,8 @@ describe Day1 do
   def test_part1
     assert_equal 24000, @d.part1
   end
+
+  def test_part2
+    assert_equal 45000, @d.part2
+  end
 end
