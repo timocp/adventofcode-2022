d6f8dcbbd93524f2b1230a713a10a72aa4970ff9
Tim Peters <tim@catminion.net>
2022-12-05 16:01:00 +1030

    Add day 5 part 1

---
+++ lib/day5.rb
@@ -0,0 +1,41 @@
+class Day5 < Base
+  def part1
+    setup
+    @instructions.each do |inst|
+      inst.moves.times do
+        @stacks[inst.to - 1].push @stacks[inst.from - 1].pop
+      end
+    end
+    @stacks.map(&:last).join
+  end
+
+  Instruction = Struct.new(:moves, :from, :to)
+
+  def setup
+    input = raw_input.split("\n\n")
+    setup_stacks(input[0])
+    setup_instructions(input[1])
+  end
+
+  def setup_stacks(input)
+    @stacks = []
+    input.split("\n").map do |row|
+      # blocks are at position 1, 5, 9, etc
+      1.step(by: 4, to: row.length).with_index do |col, i|
+        crate = row[col]
+        next unless crate.match?(/[A-Z]/)
+
+        @stacks[i] ||= []
+        @stacks[i].unshift crate
+      end
+    end
+  end
+
+  def setup_instructions(input)
+    @instructions = []
+    input.split("\n").each do |line|
+      m = line.match(/move (\d+) from (\d+) to (\d+)/)
+      @instructions << Instruction.new(m[1].to_i, m[2].to_i, m[3].to_i)
+    end
+  end
+end
---
+++ test/day5_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+describe Day5 do
+  def setup
+    @d = Day5.new
+    @d.test_input <<~TEST
+          [D]
+      [N] [C]
+      [Z] [M] [P]
+       1   2   3
+
+      move 1 from 2 to 1
+      move 3 from 1 to 3
+      move 2 from 2 to 1
+      move 1 from 1 to 2
+    TEST
+  end
+
+  def test_part1
+    assert_equal "CMZ", @d.part1
+  end
+end
