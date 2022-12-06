3b5ef7218edf1b4b28954696f7a3c10b61c012db
Tim Peters <tim@catminion.net>
2022-12-06 15:47:07 +1030

    Add day 6 part 2

--- lib/day6.rb
+++ lib/day6.rb
@@ -1,11 +1,19 @@
 class Day6 < Base
   def part1
+    input = parse_input
     0.upto(input.length) do |i|
       return i + 4 if input[i, 4].uniq.size == 4
     end
   end
 
-  def input
+  def part2
+    input = parse_input
+    0.upto(input.length) do |i|
+      return i + 14 if input[i, 14].uniq.size == 14
+    end
+  end
+
+  def parse_input
     raw_input.chomp.split("")
   end
 end
--- test/day6_test.rb
+++ test/day6_test.rb
@@ -12,4 +12,12 @@ describe Day6 do
     assert_equal 10, @d.test_input("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").part1
     assert_equal 11, @d.test_input("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").part1
   end
+
+  def test_part2
+    assert_equal 19, @d.test_input("mjqjpqmgbljsphdztnvjfqwrcgsmlb").part2
+    assert_equal 23, @d.test_input("bvwbjplbgvbhsrlpgdmjqwftvncz").part2
+    assert_equal 23, @d.test_input("nppdvjthqldpwncqszvftbrmjlhg").part2
+    assert_equal 29, @d.test_input("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").part2
+    assert_equal 26, @d.test_input("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").part2
+  end
 end
