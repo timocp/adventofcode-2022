1d96d54f319377cd321ad551bdbe91d1b0b7b94a
Tim Peters <tim@catminion.net>
2022-12-08 16:36:48 +1030

    Add day 8 part 2

--- lib/day8.rb
+++ lib/day8.rb
@@ -3,16 +3,30 @@ class Day8 < Base
     visible = Set.new
     (0...size).each do |n|
       coords = [n].product((0...size).to_a)
-      scan(coords, visible)
-      scan(coords.reverse, visible)
+      scan_from_edge(coords, visible)
+      scan_from_edge(coords.reverse, visible)
       coords = (0...size).to_a.product([n])
-      scan(coords, visible)
-      scan(coords.reverse, visible)
+      scan_from_edge(coords, visible)
+      scan_from_edge(coords.reverse, visible)
     end
     visible.size
   end
 
-  def scan(coords, visible)
+  def part2
+    (1...(size - 1)).to_a.product((1..(size - 1)).to_a).map { |row, col| scenic_score(row, col) }.max
+  end
+
+  def scenic_score(row, col)
+    h = height(row, col)
+    [
+      scan_from_tree((row - 1).downto(0).to_a.product([col]), h),
+      scan_from_tree([row].product((col - 1).downto(0).to_a), h),
+      scan_from_tree((row + 1).upto(size - 1).to_a.product([col]), h),
+      scan_from_tree([row].product((col + 1).upto(size - 1).to_a), h)
+    ].inject(&:*)
+  end
+
+  def scan_from_edge(coords, visible)
     h = -1
     coords.each do |(row, col)|
       if height(row, col) > h
@@ -20,6 +34,15 @@ class Day8 < Base
         h = height(row, col)
       end
     end
+  end
+
+  def scan_from_tree(coords, max)
+    count = 0
+    coords.each do |(row, col)|
+      count += 1
+      break if height(row, col) >= max
+    end
+    count
   end
 
   def size
--- test/day8_test.rb
+++ test/day8_test.rb
@@ -15,4 +15,11 @@ describe Day8 do
   def test_part1
     assert_equal 21, @d.part1
   end
+
+  def test_part2
+    assert_equal 1, @d.scenic_score(1, 1)
+    assert_equal 4, @d.scenic_score(1, 2)
+    assert_equal 8, @d.scenic_score(3, 2)
+    assert_equal 8, @d.part2
+  end
 end
