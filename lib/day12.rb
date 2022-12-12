4583912835b3ddfafd6a44f0ccc066ed5f89700c
Tim Peters <tim@catminion.net>
2022-12-12 18:14:11 +1030

    Speedup day 12 by searching backwards

--- lib/day12.rb
+++ lib/day12.rb
@@ -48,31 +48,29 @@ class Day12 < Base
       end
     end
 
-    def solve(start = nil)
-      queue = [start || @start]
+    def solve(backwards: false)
+      start, finish = backwards ? [@end, @start] : [@start, @end]
+      queue = [start]
       visited = { queue.first => nil }
       while (pos = queue.shift)
-        if pos == @end
+        if backwards ? elevation(pos) == 0 : pos == finish
           list = [pos]
           while (prev = visited[list.last])
             list << prev
           end
           return list.reverse
         else
-          valid_moves[pos.y][pos.x].each do |nextpos|
+          pos.each_neighbour do |nextpos|
+            next unless valid?(nextpos)
             next if visited.key?(nextpos)
+            next if backwards && elevation(nextpos) + 1 < elevation(pos)
+            next if !backwards && elevation(nextpos) > elevation(pos) + 1
 
             queue.push(nextpos)
             visited[nextpos] = pos
           end
         end
       end
-    end
-
-    def solve_best_start
-      each_pos.map do |pos|
-        solve(pos) if edge?(pos) && elevation(pos) == 0
-      end.compact.min_by(&:size)
     end
 
     def elevation(pos)
@@ -109,22 +107,6 @@ class Day12 < Base
       pos.x >= 0 && pos.x < @map.first.size && pos.y >= 0 && pos.y < @map.size
     end
 
-    def edge?(pos)
-      pos.x == 0 || pos.y == 0 || pos.x == @map.first.size - 1 || pos.y == @map.size - 1
-    end
-
-    def valid_moves
-      @valid_moves ||=
-        @map.each_with_index.map do |row, y|
-          row.each_index.map do |x|
-            pos = Pos.new(x, y)
-            pos.each_neighbour.select do |nextpos|
-              valid?(nextpos) && elevation(nextpos) <= elevation(pos) + 1
-            end
-          end
-        end
-    end
-
     def parse_cell(cell)
       case cell
       when "S" then 0
@@ -140,7 +122,7 @@ class Day12 < Base
   end
 
   def part2
-    parse_input.solve_best_start.size - 1
+    parse_input.solve(backwards: true).size - 1
   end
 
   def parse_input
