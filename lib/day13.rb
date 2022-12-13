58a8fb62f1cace016ae1b2bb6969a92080610d3e
Tim Peters <tim@catminion.net>
2022-12-13 16:34:17 +1030

    Add day 13 part 2

--- lib/day13.rb
+++ lib/day13.rb
@@ -6,6 +6,22 @@ class Day13 < Base
                .sum
   end
 
+  DIVIDER1 = [[2]].freeze
+  DIVIDER2 = [[6]].freeze
+
+  def part2
+    packets = (parse_input.map(&:first) + parse_input.map(&:last) + [DIVIDER1, DIVIDER2]).sort do |a, b|
+      if a == b
+        0
+      elsif correct_order?(a, b)
+        -1
+      else
+        1
+      end
+    end
+    (packets.index(DIVIDER1) + 1) * (packets.index(DIVIDER2) + 1)
+  end
+
   def correct_order?(left, right, depth = 1)
     # puts if depth == 1
     # print "- ".rjust(depth * 2, " ")
@@ -15,15 +31,17 @@ class Day13 < Base
       result = true if left < right
       result = false if left > right
     elsif left.is_a?(Array) && right.is_a?(Array)
-      left.zip(right).each do |l, r|
-        if r.nil?
+      0.upto([left.size, right.size].max) do |i|
+        if right.size < i
           result = false
           break
+        elsif left.size < i
+          result = true
+          break
         end
-        result = correct_order?(l, r, depth + 1)
-        break unless result.nil?
+        result = correct_order?(left[i], right[i])
+        return result unless result.nil?
       end
-      result = true if result.nil? && left.size <= right.size
     elsif left.is_a?(Array) && right.is_a?(Integer)
       result = correct_order?(left, Array(right), depth + 1)
     elsif left.is_a?(Integer) && right.is_a?(Array)
--- test/day13_test.rb
+++ test/day13_test.rb
@@ -33,4 +33,34 @@ describe Day13 do
   def test_part1
     assert_equal 13, @d.part1
   end
+
+  def test_correct_order?
+    [
+      [],
+      [[]],
+      [[[]]],
+      [1, 1, 3, 1, 1],
+      [1, 1, 5, 1, 1],
+      [[1], [2, 3, 4]],
+      [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+      [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+      [[1], 4],
+      [[2]],
+      [3],
+      [[4, 4], 4, 4],
+      [[4, 4], 4, 4, 4],
+      [[6]],
+      [7, 7, 7],
+      [7, 7, 7, 7],
+      [[8, 7, 6]],
+      [9]
+    ].each_cons(2) do |a, b|
+      assert @d.correct_order?(a, b)
+      refute @d.correct_order?(b, a)
+    end
+  end
+
+  def test_part2
+    assert_equal 140, @d.part2
+  end
 end
